99e7a75e60dea85638da4710975b31c898000acf
nzrjedGitHub <98401179+nzrjedGitHub@users.noreply.github.com>
2024-03-23 22:16:13 +0200

    first commit

---
+++ shooter_game.py
@@ -0,0 +1,106 @@
+from pygame import *
+from random import randint
+
+mixer.init()
+mixer.music.load('space.ogg')
+mixer.music.play()
+fire_sound = mixer.Sound('fire.ogg')
+
+font.init()
+font1 = font.Font(None, 36)
+
+img_back = "galaxy.jpg"
+img_hero = "rocket.png"
+img_enemy = "ufo.png"
+
+score = 0
+lost = 0
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, sprite_img, sprite_x, sprite_y, size_x, sixe_y , sprite_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(sprite_img),(size_x, sixe_y))
+        self.speed = sprite_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = sprite_x
+        self.rect.y = sprite_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+    def fire(self):
+        bullet = Bullet("bullet.png", self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
+
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+        if self.rect.y > win_height:
+            self.rect.x = randint (80, win_width - 80)
+            self.rect.y = 0
+            lost +=1
+
+class Bullet(GameSprite):
+    # рух ворога
+    def update(self):
+        self.rect.y += self.speed
+        # зникає, якщо дійде до краю екрана
+        if self.rect.y < 0:
+            self.kill()
+
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption("Shooter")
+background = transform.scale(image.load(img_back), (win_width, win_height))
+
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
+
+bullets = sprite.Group()
+monsters = sprite.Group()
+for i in range(1, 6):
+    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 3))
+    monsters.add(monster)
+
+run = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
+
+    if not finish:
+        window.blit(background, (0, 0))
+
+        text = font1.render("Рахунок" + str(score),1, (255,255,255))
+        window.blit(text,(10, 20))
+
+        text_lose = font1.render("Пропущенно:" + str(lost),1, (255, 255, 255))
+        window.blit(text_lose, (10, 50))
+
+        ship.update()
+        monsters.update()
+        bullets.update()
+
+        ship.reset()
+        monsters.draw(window)
+        bullets.draw(window)
+
+        display.update()
+
+    clock.tick(FPS)
